c6e8ff617b394da97605b3412cf871874ea1d858
張茜雯 <112846217+JennyChang1124@users.noreply.github.com>
2022-11-11 20:03:00 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:photo_view/photo_view.dart';
+import 'B.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = '瀏覽照片';
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
       home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatelessWidget(),
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          backgroundColor: Colors.green,
+        ),
+        body: HomePage(),
       ),
     );
   }
 }
-/// stateless widget that the main application instantiates
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
 
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-        const SizedBox(height: 30),
-    const SizedBox(height: 10),
-    Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      FloatingActionButton(
-        onPressed: (){},
-        child:const Icon(Icons.keyboard_arrow_left),
-      ),
-      FloatingActionButton(
-        onPressed: (){},
-        child:const Icon(Icons.keyboard_arrow_right),
-      ),
-    ]
-    ),
-      ]
-    ),
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 2,
+      children: <Widget>[
+        ElevatedButton(
+          child: const Icon(Icons.menu_book),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => BPage())
+            );
+          },
+        ),
+        const Expanded(
+          child: FittedBox(
+            child: FlutterLogo(),
+          ),
+        ),
+      ],
     );
+
   }
 }
